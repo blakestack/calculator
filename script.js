e70dff4094979418f87090685330b9b320400261
Blake Davis <Blakedav2088@gmail.com>
2021-02-20 05:50:08 -0500

    Added divs to the page for the calculator buttons and tried to add numbers to butons

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const calculatorBtns = document.querySelector('calculatorButtons');
-l
+//add calulator divs to page
+function addButtonsToPage() {
+  const buttonsDiv = document.querySelector(".calculatorButtons");
+  for (let i = 0; i < 18; i++) {
+    const newDiv = document.createElement("div");
+    buttonsDiv.appendChild(newDiv);
+  }
+}
+
+addButtonsToPage();
+
+//add numbers and symbols to buttons
+const buttonsDiv = document.querySelector('.calculatorButtons');
+const calcButtons = buttonsDiv.querySelectorAll('div');
+calcButtons.forEach((div) => {
+    div.textContent = 1;
+});
+
+
