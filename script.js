23be34f8910299d352167a2099718846ac9c27ca
Blake Davis <Blakedav2088@gmail.com>
2021-03-18 03:00:12 -0400

    added functionality to add decimals to the numbers, also added functionality to make numbers negative and positive

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let currentValue = "";
 //functions for the calculator operations
 
 function add(arg1, arg2) {
-  return parseFloat(arg1) + parseFloat(arg2);
+  return arg1 + arg2;
 }
 
 function subtract(arg1, arg2) {
@@ -48,7 +48,7 @@ function operate(operator, arg1, arg2) {
 function calculate() {
   //arr.forEach((element) => (equation += element + " "));
   arr.push(currentValue);
-  let answer = operate(arr[1], parseInt(arr[0]), parseInt(arr[2]));
+  let answer = operate(arr[1], parseFloat(arr[0]), parseFloat(arr[2]));
   currentValue = "";
   addToCurrentValue(answer);
   arr = [];
@@ -81,25 +81,47 @@ function clearCalculator() {
   currentValue = "";
 }
 
-
-function addDecimal(){
-  //let newNumber = "." + currentValue.slice(0, currentValue.length);
+function addDecimal() {
   currentValue += ".";
+  updateDisplay();
+}
+
+function addNegative() {
+  let firstChar = currentValue.charAt(0);
+  console.log(firstChar);
+  if (firstChar === "-") {
+    //split current value into and array and remove the first character
+    let charArray = currentValue.split("");
+    charArray.shift();
+    let newValue = "";
+    //for loop to make new string
+    for (let i = 0; i < charArray.length; i++) {
+      newValue += charArray[i];
+    } //end of for loop
+
+    //assign the new current value
+    currentValue = newValue;
+  } else {
+    currentValue = "-" + currentValue.slice(0, currentValue.length);
+  }
   updateDisplay();
 }
 
 //adding functionality to buttons
 
+//number buttons
+
 const numberButtons = document.querySelectorAll(".numberBtn");
 
 numberButtons.forEach((number) => {
-  if(number.textContent === "."){
-    number.addEventListener('click', addDecimal);
-  }else{
+  if (number.textContent === ".") {
+    number.addEventListener("click", addDecimal);
+  } else {
     number.setAttribute("onclick", `addToCurrentValue(${number.textContent})`);
   }
-  
 });
+
+//operator buttons
 
 const operatorButtons = document.querySelectorAll(".operatorBtn");
 
@@ -108,6 +130,8 @@ operatorButtons.forEach((operator) => {
     operator.addEventListener("click", calculate);
   } else if (operator.textContent === "AC") {
     operator.addEventListener("click", clearCalculator);
+  } else if (operator.textContent === "+/-") {
+    operator.addEventListener("click", addNegative);
   } else {
     operator.setAttribute("onclick", `addToArray('${operator.textContent}')`);
     //operator.addEventListener('clock', addToArray);
